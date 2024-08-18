71a89eb521c572e65d346abcfa68e2e68ec25b40
Maxime Danic <maxime.danic@gmail.com>
2024-08-18 15:45:23 +0300

    chore: Update redux store imports to use single quotes and consistent formatting

--- Front-end/argentbank/src/redux/store.js
+++ Front-end/argentbank/src/redux/store.js
@@ -1,16 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authSlice from "../features/auth/authSlice";
-import userProfileSlice from "../features/auth/userProfileSlice";
-import { apiSlice } from "../features/api/apiSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import authSlice from '../features/auth/authSlice';
+import userProfileSlice from '../features/auth/userProfileSlice';
+import { apiSlice } from '../features/api/apiSlice';
 
 // persisting the store
 
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import { combineReducers } from "@reduxjs/toolkit";
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from '@reduxjs/toolkit';
 
 const persistConfig = {
-  key: "root",
+  key: 'root',
   version: 1,
   storage,
 };
