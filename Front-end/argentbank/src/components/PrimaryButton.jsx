44ee4bc3e39bf6d704632f21318f68e4b9e1026a
Maxime Danic <maxime.danic@gmail.com>
2024-08-18 16:25:33 +0300

    Update the styles of the PrimaryButton component to improve its appearance and hover effects.

--- Front-end/argentbank/src/components/PrimaryButton.jsx
+++ Front-end/argentbank/src/components/PrimaryButton.jsx
@@ -1,7 +1,10 @@
-const hoverEffects = {
+const buttonStyles = {
+  default:
+    'border-secondary bg-secondary p-[8px] text-[1.1rem] font-semibold text-white hover:bg-secondary/80',
   border:
-    'hover:border-b-secondary hover:border-l-green-800 hover:border-r-secondary hover:border-t-green-800',
-  background: 'hover:bg-secondary/80',
+    'border-2 border-b-green-800 border-l-secondary border-r-green-800 border-t-secondary bg-secondary p-[10px] text-sm font-bold text-white duration-150 ease-in-out hover:border-b-secondary hover:border-l-green-800 hover:border-r-secondary hover:border-t-green-800',
+  borderSecondary:
+    'border-2 border-b-green-800 border-l-secondary border-r-green-800 border-t-secondary bg-secondary p-[8px] text-[1.1rem] font-bold text-white duration-150 ease-in-out hover:border-b-secondary hover:border-l-green-800 hover:border-r-secondary hover:border-t-green-800 sm:px-6',
 };
 
 const PrimaryButton = ({
@@ -9,14 +12,14 @@ const PrimaryButton = ({
   type,
   onClick,
   className,
-  hoverEffect = 'background',
+  buttonStyle = 'default',
 }) => {
-  const hoverClass = hoverEffects[hoverEffect] || hoverEffects['background'];
+  const hoverClass = buttonStyles[buttonStyle] || buttonStyles['default'];
   return (
     <button
       onClick={onClick}
       type={type}
-      className={` border-secondary bg-secondary p-[8px] text-[1.1rem] font-semibold text-white ${hoverClass} ${className}`}
+      className={`${hoverClass} ${className}`}
     >
       {label}
     </button>
